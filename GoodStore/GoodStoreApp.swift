6d03843361cc556f8896a6e891e02c85b94d0b51
Mohammad Azam <azamsharp@gmail.com>
2022-03-01 14:36:13 -0600

    Initial Commit

---
+++ GoodStore/GoodStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodStoreApp.swift
+//  GoodStore
+//
+//  Created by Mohammad Azam on 3/1/22.
+//
+
+import SwiftUI
+
+@main
+struct GoodStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
